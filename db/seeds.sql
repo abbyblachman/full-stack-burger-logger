d1cd08dd0f0e1e818a916191061d604cdd46ea00
Abby Blachman <ablachman@Abbys-MacBook-Pro.local>
2019-12-19 21:08:52 -0600

    working version

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("beyond", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("classic", FALSE);
